368ee791f571b76298a38f25bbb823da8fd716d9
dinhlc <dinhlc@vnext.vn>
2024-03-14 00:28:19 +0700

    update

--- src/components/Bridegroom.js
+++ src/components/Bridegroom.js
@@ -4,8 +4,8 @@ import Groom from "../assets/images/phuong.jpg";
 function Bridegroom() {
   return (
     <div id="couple" className="bridegroom clear section-padding bg-pink">
-      <div className="container">
-        <div className="row mb-60">
+      <div className="">
+        {/* <div className="row mb-60">
           <div className="col-md-6">
             <div
               className="item toright mb-30 animate-box h-100"
@@ -56,6 +56,61 @@ function Bridegroom() {
                   </p>
                 </div>
               </div>
+            </div>
+          </div>
+        </div> */}
+        <div
+          className="d-flex flex-column"
+          style={{ backgroundColor: "white", padding: "0 50px" }}
+        >
+          <div className="d-flex">
+            <div className="w-50 d-flex align-items-center">
+              <img
+                src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/4c66f96d92838fa22302b6cd9a16eac0.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="w-50">
+              <div className="info valign m-0 p-3">
+                <div className="full-width">
+                  <h6>
+                    Đỗ Hữu Phát <i className="ti-heart"></i>
+                  </h6>{" "}
+                  <span>29/10/1999</span>
+                  <p>
+                    Chàng trai IT với nhiều ước mơ và hoài bão. Một chàng trai
+                    kiên trì, bền bỉ trong mọi quyết định của bản thân. Giao
+                    diện “trẻ con” nhưng có hệ điều hành cực kỳ tình cảm, biết
+                    lắng nghe và rất yêu thích bộ môn trêu vợ.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className="w-50">
+              <div className="info valign m-0 p-3">
+                <div className="full-width">
+                  <h6>
+                    Chu Thị Linh Phương <i className="ti-heart"></i>
+                  </h6>{" "}
+                  <span>08/03/1998</span>
+                  <p>
+                    Cô gái IT Comtor (biên - phiên dịch Tiếng Nhật) , đang học
+                    tập và làm việc với vị trí Tester. Là cô gái không thích
+                    giậm chân tại chỗ, độc lập và luôn hướng đến mục tiêu do bản
+                    thân đặt ra. Đôi lúc ít nói, nhiều lúc nói không ai bằng.
+                    Thân thiện, hay cười, cực kỳ yêu trẻ con và động vật (đặc
+                    biệt là chó)
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-50 d-flex align-items-center">
+              <img
+                src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/ba65a9e4fc11f7f65d55fa0f8e12af9a.jpeg"
+                alt=""
+              />
             </div>
           </div>
         </div>
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,71 +1,264 @@
-import React from 'react'
-import storyImage from '../assets/images/anhdoi.jpg'
-function Story () {
+import React from "react";
+import storyImage from "../assets/images/anhdoi.jpg";
+function Story() {
   return (
-    <div id='story' className='story section-padding'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-5 mb-30'>
-            <div className='story-img animate-box'>
-              <div className='img'>
-                {' '}
-                <img src={storyImage} className='img-fluid' alt='' />{' '}
+    <div id="story" className="story section-padding">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 mb-30">
+            <div className="story-img animate-box">
+              <div className="img">
+                {" "}
+                <img src={storyImage} className="img-fluid" alt="" />{" "}
               </div>
-              <div
-                className='story-img-2 story-wedding'
-              ></div>
+              <div className="story-img-2 story-wedding"></div>
             </div>
           </div>
-          <div className='col-md-7 animate-box'>
-            <h4 className='oliven-story-subtitle'>Our love.</h4>
-            <h3 className='oliven-story-title'>Our Story</h3>
-            <h5>
-            * March 2, 2020 ~ Lần đầu gặp mặt
-            </h5>
+          <div className="col-md-7 animate-box">
+            <h4 className="oliven-story-subtitle">Our love.</h4>
+            <h3 className="oliven-story-title">Our Story</h3>
+            <h5>* March 2, 2020 ~ Lần đầu gặp mặt</h5>
             <p>
-Tụi mình gặp nhau lần đầu tiên tại công ty NSMV, nơi mà hai đứa đang xin vào làm thực tập.
+              Tụi mình gặp nhau lần đầu tiên tại công ty NSMV, nơi mà hai đứa
+              đang xin vào làm thực tập.
             </p>
+            <p>+ Ấn tượng đầu tiên của mình về cô ấy :………….</p>
             <p>
-            + Ấn tượng đầu tiên của mình về cô ấy :………….
+              + Ấn tượng đầu tiên của mình về anh ấy : trẻ con, nói nhiều, hay
+              trêu mình và nhờ mình dịch và dạy tiếng nhật rất nhiều.
             </p>
+            <h5>* June 28, 3020 ~ Chúng mình thành một cặp</h5>
             <p>
-+ Ấn tượng đầu tiên của mình về anh ấy : trẻ con, nói nhiều, hay trêu mình và nhờ mình dịch và dạy tiếng nhật rất nhiều. 
+              Sau chuyến du lịch cùng công ty hồi tháng 6, mình chấp nhận lời
+              hẹn hò từ đằng trai. Và thời gian cứ thế trôi , chúng mình chính
+              thức thành 1 cặp, trong sự ngỡ ngàng ngơ ngác từ các anh - chị -
+              em - bạn bè cùng công ty.
             </p>
-            <h5>
-            * June 28, 3020 ~ Chúng mình thành một cặp
-            </h5>
+            <h5>* January 2021 ~ Yêu xa có thật sự là thử thách</h5>
             <p>
-            Sau chuyến du lịch cùng công ty hồi tháng 6, mình chấp nhận lời hẹn hò từ đằng trai. Và thời gian cứ thế trôi , chúng mình chính thức thành 1 cặp, trong sự ngỡ ngàng ngơ ngác từ các anh - chị - em - bạn bè cùng công ty.
-            </p>
-            <h5>
-            * January 2021 ~ Yêu xa có thật sự là thử thách
-            </h5>
-            <p>
-            Chúng mình yêu nhau được khoảng 5 tháng thì mình nhận được thông báo sang Nhật thực tập 1 năm. 
-            Mình khá lo ngại nhưng anh chỉ nói một câu : “Anh đợi em về”
-            Vậy là chúng mình bắt đầu cuộc hành trình 365 ngày xa nhau.
-            Một năm có thể không dài nhưng nó vẫn là một thử thách có độ khó nhất định cho cả hai tụi mình.
-            <p>……</p>
-            <p>Và tụi mình đã cùng nhau trải qua thử thách Yêu xa</p>
-            <h5>* January 2022 - Now ~ Là khoảng thời gian anh Thực hiện lời hứa</h5>
-            <p>
-            Do tính chất công việc bên Nhật, nên mình gần như không có thời gian để đi đây đi đó, mình khá buồn, và khi đó anh nói “ Về Việt Nam anh sẽ cho em đi những nơi mà em muốn “ 
-            Tháng 1 năm 2022, mình trở về Việt Nam cùng anh thực hiện lời hứa
-            và anh đã đưa mình đi : …….
-            <h5>* March 8, 2024 ~ Phút giây cầu hôn</h5>
-            <p>
-            Mình không nhớ là mình từng nói : ”Em muốn được cầu hôn”, chỉ biết là anh đã lắng nghe mong muốn của mình và thực hiện nó vào đúng ngày sinh nhật của mình. Mình cảm thấy rất bất ngờ và vô cùng hạnh phúc 
-            </p>
-            <h5>* March 31, 2024 ~ Chúng mình cưới nhau</h5>
-            <p>Sau …. Năm…. Tháng ….. Ngày …… tán tỉnh - hẹn hò - yêu xa - gặp lại chúng mình sẽ có một đám cưới vào 31/3/2024. Cùng nhau xây dựng một gia đình nhỏ xinh, cùng nhau tạo nên những điều tuyệt vời …</p>
-            <p>Cùng đến tham dự, chung vui cùng tụi mình nhé</p>
-            </p>
+              Chúng mình yêu nhau được khoảng 5 tháng thì mình nhận được thông
+              báo sang Nhật thực tập 1 năm. Mình khá lo ngại nhưng anh chỉ nói
+              một câu : “Anh đợi em về” Vậy là chúng mình bắt đầu cuộc hành
+              trình 365 ngày xa nhau. Một năm có thể không dài nhưng nó vẫn là
+              một thử thách có độ khó nhất định cho cả hai tụi mình.
+              <p>……</p>
+              <p>Và tụi mình đã cùng nhau trải qua thử thách Yêu xa</p>
+              <h5>
+                * January 2022 - Now ~ Là khoảng thời gian anh Thực hiện lời hứa
+              </h5>
+              <p>
+                Do tính chất công việc bên Nhật, nên mình gần như không có thời
+                gian để đi đây đi đó, mình khá buồn, và khi đó anh nói “ Về Việt
+                Nam anh sẽ cho em đi những nơi mà em muốn “ Tháng 1 năm 2022,
+                mình trở về Việt Nam cùng anh thực hiện lời hứa và anh đã đưa
+                mình đi : …….
+                <h5>* March 8, 2024 ~ Phút giây cầu hôn</h5>
+                <p>
+                  Mình không nhớ là mình từng nói : ”Em muốn được cầu hôn”, chỉ
+                  biết là anh đã lắng nghe mong muốn của mình và thực hiện nó
+                  vào đúng ngày sinh nhật của mình. Mình cảm thấy rất bất ngờ và
+                  vô cùng hạnh phúc
+                </p>
+                <h5>* March 31, 2024 ~ Chúng mình cưới nhau</h5>
+                <p>
+                  Sau …. Năm…. Tháng ….. Ngày …… tán tỉnh - hẹn hò - yêu xa -
+                  gặp lại chúng mình sẽ có một đám cưới vào 31/3/2024. Cùng nhau
+                  xây dựng một gia đình nhỏ xinh, cùng nhau tạo nên những điều
+                  tuyệt vời …
+                </p>
+                <p>Cùng đến tham dự, chung vui cùng tụi mình nhé</p>
+              </p>
             </p>
           </div>
         </div>
       </div>
+
+      <div className="">
+        <section class="story-section section-padding" id="story">
+          <div class="container">
+            <div class="row">
+              <div class="col col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                <div class="section-title">
+                  <h2>Chuyện Tình Yêu</h2>
+                  <p>
+                    Khi bạn nhận ra rằng mình muốn chia sẻ quãng đời còn lại với
+                    một ai đó, bạn sẽ mong điều đó sớm bắt đầu.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <div class="story-block">
+                  <ul>
+                    <li>
+                      <div class="details">
+                        <h3>Lần đầu gặp gỡ</h3>
+                        <span class="date">2023</span>
+                        <p>
+                          Tôi nhớ hôm đó là vào 1 ngày đẹp trời, một cô gái tham
+                          gia vào công ty tôi đang làm và không nghĩ đó lại là
+                          định mệnh của cuộc đời mình. Ấn tượng về cô gái đó
+                          không đến một cách mạnh mẽ như những câu chuyện tình
+                          cảm tôi hay đọc trên mạng mà nó đến từ từ rồi ngấm dần
+                          vào trái tim tôi. Một cô gái nhỏ nhắn, xinh xắn và
+                          cùng quê với tôi. Sau một thời gian làm việc cùng thì
+                          thấy cô gái đó có khá nhiều tài lẻ: hát hay, đàn giỏi
+                          và đặc biệt là khá chăm chỉ, chịu khó lại còn nấu ăn
+                          ngon nữa. Đàn ông chúng ta chắc chắn sẽ bị đánh gục
+                          bởi những cô gái nấu ăn ngon như vậy. Tôi đã chính
+                          thức FALL IN LOVE và bắt đầu theo đuổi cô gái ấy.
+                          <br />
+                          ***----------***
+                          <br />
+                          Lần đầu tiên gặp gỡ <br />
+                          giữa hai ta là ngày em bắt đầu làm việc tại công ty.
+                          Thời gian đầu chưa làm việc nhiều với anh thấy: Anh
+                          hiền, khá chăm chỉ, khó tính, nhìn ghét ghét, tính
+                          cách thì trái ngược em 🙃. Em chưa từng nghĩ sẽ quen
+                          và anh sẽ là người bạn đời của em cả 😵,,...
+                          <br />
+                          Vì thế: “Hãy để tình yêu diễn biến thật tự nhiên, đã
+                          là duyên thì chạy cũng chẳng thoát.”
+                        </p>
+                      </div>
+                      <div class="img-holder">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/11e458b9b8abfce6643e6fca099f52ca.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="img-holder img-holder-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/779d6902404ecfe7672397ecce01413e.jpeg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="details">
+                        <h3>Lời tỏ tình đầu tiên</h3>
+                        <span class="date">2023</span>
+                        <p>
+                          Mối quan hệ đôi ta chính thức bắt đầu có lẽ nhờ chuyến
+                          đi du lịch định mệnh ấy - trên vùng đất cảng miền Nam
+                          Trung Bộ, Quy Nhơn. Anh và em đã có nhiều cơ hội, thời
+                          gian hơn để trò chuyện, chia sẻ và bày tỏ cùng nhau.{" "}
+                          <br />
+                          Dưới ánh trăng sáng, trên bờ cát trắng, sóng biển rì
+                          rào thì thào anh nói nhỏ: "Mình tìm hiểu nhau nhé". Và
+                          tình yêu của chúng tôi bắt đầu như vậy đó!
+                        </p>
+                      </div>
+                      <div class="img-holder img-show-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/779d6902404ecfe7672397ecce01413e.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="details">
+                        <h3>Tháng ngày thử thách</h3>
+                        <span class="date">2023</span>
+                        <p>
+                          Thời gian tìm hiểu, yêu và quyết định đi cùng nhau có
+                          lẽ không dài cũng chẳng ngắn. Có rất nhiều lần tưởng
+                          chừng như là dấu chấm hết cho một mối tình. Cãi vã,
+                          giận hờn, bất đồng .... Nhưng bằng tình yêu chân
+                          thành, sự cảm thông thấu hiểu, cương - nhu tuỳ lúc
+                          chúng ta vẫn tiếp tục để cùng nhau sánh bước.
+                        </p>
+                      </div>
+                      <div class="img-holder">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/55d8aef4f4eb48730cbd6f9d0b21e70f.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="img-holder img-holder-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/24683f400e29914ea6ca24c4c2db6c5a.jpeg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="details">
+                        <h3>Lời cầu hôn</h3>
+                        <span class="date">2023-2024</span>
+                        <p>
+                          Ngày tháng bên nhau cho chúng ta nhận ra được rất
+                          nhiều điều. Chúng ta từ 2 con người xa lạ mà bước vào
+                          cuộc đời nhau. Yêu nhau, vun vén hạnh phúc, chia sẻ và
+                          cùng nỗ lực vượt qua những khó khăn trong cuộc sống.
+                          Giây phút anh ngỏ lời “Làm vợ anh nhé!” dưới sự chứng
+                          kiến của mọi người, đặc biệt là anh chị em trong công
+                          ty - nơi chúng ta làm việc, nơi khơi nguồn tình yêu và
+                          từ đây cũng bắt đầu cho cuộc sống mới của hai ta. Vì
+                          sự quan tâm nhẹ nhàng và chân thành của anh, em đã
+                          nguyện ý cùng anh vun vén hạnh phúc thêm nhiều lần 10
+                          năm nữa.
+                        </p>
+                      </div>
+                      <div class="img-holder img-show-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/24683f400e29914ea6ca24c4c2db6c5a.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="details">
+                        <h3>Dạm ngõ</h3>
+                        <span class="date">2023</span>
+                        <p>
+                          Được sự đồng thuận của bố mẹ, họ hàng hai bên gia đình
+                          thì cuối cùng ngày vui của chúng ta cũng tới gần. Cảm
+                          ơn vì mình đã luôn là một phần trong cuộc sống của
+                          nhau.
+                        </p>
+                      </div>
+                      <div class="img-holder">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/33a4d5e26777054ebafc4d1d9d360c2f.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                    <li>
+                      <div class="img-holder img-holder-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/0ffc5eac97c3c9c563b33823160b6ea2.jpeg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="details">
+                        <h3>Coming Soon</h3>
+                        <span class="date">2024</span>
+                        <p>
+                          Sau tất cả thì :"Tình yêu lớn không phải là yêu nhiều
+                          người mà là yêu một người và yêu suốt cuộc đời". Hãy
+                          chúc phúc và chung vui cùng vợ chồng mình nhé......
+                          Coming Soon!
+                        </p>
+                      </div>
+                      <div class="img-holder img-show-hideinmobile">
+                        <img
+                          src="https://cdn.biihappy.com/ziiweb/website/64c1cd63229536be250cff58/0ffc5eac97c3c9c563b33823160b6ea2.jpeg"
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Story
+export default Story;

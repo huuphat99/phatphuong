6e237796ea71d875a6a706fc7ed508d549329768
Phat Dev <phuongphatwedding@gmail.com>
2024-03-14 00:34:43 +0700

    Update

--- src/components/Bridegroom.js
+++ src/components/Bridegroom.js
@@ -1,67 +1,10 @@
 import React from "react";
-import Bride from "../assets/images/phat.jpg";
-import Groom from "../assets/images/phuong.jpg";
 function Bridegroom() {
   return (
     <div id="couple" className="bridegroom clear section-padding bg-pink">
-      <div className="">
-        {/* <div className="row mb-60">
-          <div className="col-md-6">
-            <div
-              className="item toright mb-30 animate-box h-100"
-              data-animate-effect="fadeInLeft"
-            >
-              <div className="img">
-                {" "}
-                <img src={Bride} alt="" />{" "}
-              </div>
-              <div className="info valign">
-                <div className="full-width">
-                  <h6>
-                    Đỗ Hữu Phát <i className="ti-heart"></i>
-                  </h6>{" "}
-                  <span>29/10/1999</span>
-                  <p>
-                    Chàng trai IT với nhiều ước mơ và hoài bão. Một chàng trai
-                    kiên trì, bền bỉ trong mọi quyết định của bản thân. Giao
-                    diện “trẻ con” nhưng có hệ điều hành cực kỳ tình cảm, biết
-                    lắng nghe và rất yêu thích bộ môn trêu vợ.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div
-              className="item mb-30 animate-box h-100"
-              data-animate-effect="fadeInRight"
-            >
-              <div className="img">
-                {" "}
-                <img src={Groom} alt="" />{" "}
-              </div>
-              <div className="info valign">
-                <div className="full-width">
-                  <h6>
-                    Chu Thị Linh Phương <i className="ti-heart"></i>
-                  </h6>{" "}
-                  <span>08/03/1998</span>
-                  <p>
-                    Cô gái IT Comtor (biên - phiên dịch Tiếng Nhật) , đang học
-                    tập và làm việc với vị trí Tester. Là cô gái không thích
-                    giậm chân tại chỗ, độc lập và luôn hướng đến mục tiêu do bản
-                    thân đặt ra. Đôi lúc ít nói, nhiều lúc nói không ai bằng.
-                    Thân thiện, hay cười, cực kỳ yêu trẻ con và động vật (đặc
-                    biệt là chó)
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div
           className="d-flex flex-column"
-          style={{ backgroundColor: "white", padding: "0 50px" }}
+          style={{ backgroundColor: "white", padding: "20px 50px" }}
         >
           <div className="d-flex">
             <div className="w-50 d-flex align-items-center">
@@ -125,7 +68,6 @@ function Bridegroom() {
             </h4>
           </div>
         </div>
-      </div>
     </div>
   );
 }

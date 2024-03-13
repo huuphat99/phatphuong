eeb1827fc76ab7474308fb0ad4c2ed8a8369b93e
dinhlc <dinhlc@vnext.vn>
2024-03-13 23:30:03 +0700

    update

--- src/components/Bridegroom.js
+++ src/components/Bridegroom.js
@@ -8,7 +8,7 @@ function Bridegroom() {
         <div className="row mb-60">
           <div className="col-md-6">
             <div
-              className="item toright mb-30 animate-box"
+              className="item toright mb-30 animate-box h-100"
               data-animate-effect="fadeInLeft"
             >
               <div className="img">
@@ -21,15 +21,19 @@ function Bridegroom() {
                     Đỗ Hữu Phát <i className="ti-heart"></i>
                   </h6>{" "}
                   <span>29/10/1999</span>
-                  <p>Chàng trai IT với nhiều ước mơ và hoài bão.
-                  Một chàng trai kiên trì, bền bỉ trong mọi quyết định của bản thân. Giao diện “trẻ con” nhưng có hệ điều hành cực kỳ tình cảm, biết lắng nghe và rất yêu thích bộ môn trêu vợ.</p>
+                  <p>
+                    Chàng trai IT với nhiều ước mơ và hoài bão. Một chàng trai
+                    kiên trì, bền bỉ trong mọi quyết định của bản thân. Giao
+                    diện “trẻ con” nhưng có hệ điều hành cực kỳ tình cảm, biết
+                    lắng nghe và rất yêu thích bộ môn trêu vợ.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-md-6">
             <div
-              className="item mb-30 animate-box"
+              className="item mb-30 animate-box h-100"
               data-animate-effect="fadeInRight"
             >
               <div className="img">
@@ -42,9 +46,14 @@ function Bridegroom() {
                     Chu Thị Linh Phương <i className="ti-heart"></i>
                   </h6>{" "}
                   <span>08/03/1998</span>
-                  <p>Cô gái IT Comtor (biên - phiên dịch Tiếng Nhật) , đang học tập và làm việc với vị trí Tester. Là cô gái không thích giậm chân tại chỗ, độc lập và luôn hướng đến mục tiêu do bản thân đặt ra.
-                    Đôi lúc ít nói, nhiều lúc nói không ai bằng.
-                    Thân thiện, hay cười, cực kỳ yêu trẻ con và động vật (đặc biệt là chó)</p>
+                  <p>
+                    Cô gái IT Comtor (biên - phiên dịch Tiếng Nhật) , đang học
+                    tập và làm việc với vị trí Tester. Là cô gái không thích
+                    giậm chân tại chỗ, độc lập và luôn hướng đến mục tiêu do bản
+                    thân đặt ra. Đôi lúc ít nói, nhiều lúc nói không ai bằng.
+                    Thân thiện, hay cười, cực kỳ yêu trẻ con và động vật (đặc
+                    biệt là chó)
+                  </p>
                 </div>
               </div>
             </div>

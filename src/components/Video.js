67da4fc2a6c1eaa1354f9a2354108ada37ab77fc
Phat Dev <phuongphatwedding@gmail.com>
2024-03-23 00:27:20 +0700

    url video

--- src/components/Video.js
+++ src/components/Video.js
@@ -15,7 +15,7 @@ function Video() {
         <iframe
           width="100%"
           height="700"
-          src="https://www.youtube.com/embed/oqNus0Bw9MU?si=KCUX5mGJOVhPRwxj"
+          src="https://www.youtube.com/embed/Oc_GNGtEw4A?si=X3ZHiAwNZgzVww4U"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
